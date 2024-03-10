3cc3f0918801deb69508e096e91f4d3b87284778
hknhtpgl48 <hkn.hatipolu48@gmail.com>
2024-03-10 09:46:40 +0300

    Initial Commit

---
+++ SwiftUIWithAlamofire/SwiftUIWithAlamofireApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIWithAlamofireApp.swift
+//  SwiftUIWithAlamofire
+//
+//  Created by Hakan Hatipoğlu on 10.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIWithAlamofireApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
